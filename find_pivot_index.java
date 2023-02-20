d3ebfebc5df205c75e0bf91d7b1305ed37581eab
Phani Sai Ram Munipalli <34421500+phanisaimunipalli@users.noreply.github.com>
2023-02-19 20:16:07 -0800

    Fixed an increment length

--- find_pivot_index.java
+++ find_pivot_index.java
@@ -12,7 +12,7 @@ class Solution {
             totalSum += n;
         }
 
-        for(int i = 0; i < nums.length - 1; i++){
+        for(int i = 0; i < nums.length; i++){
             if(leftSum == totalSum - leftSum - nums[i]){ // (leftSum == rightSum)
                 return i;
             }
