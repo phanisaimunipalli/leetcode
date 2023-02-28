74fa854665177e7756c7e6f012dea161854500d7
Phani Sai Ram Munipalli <34421500+phanisaimunipalli@users.noreply.github.com>
2023-02-27 18:11:35 -0800

    Updated First bad version with comments

--- first_bad_version.java
+++ first_bad_version.java
@@ -4,12 +4,16 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         // binary search to find that bad version
+        // consider this as a sorted array 
+        // [1      2     3     4     5]
+        //  F      F     F     T     T
         int low = 1;
         int high = n;
         int mid; 
         
         while(low <= high){
             mid = low + (high-low)/2;   // if the mid value becomes higher than actual lenght of array
+            // IMP: focus on always mid 
             if(isBadVersion(mid) == false){
                 low = mid + 1;
             }else{
